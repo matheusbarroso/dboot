ef99fb77c011975437372444c783a5d43bc3fdbf
matheusbarroso <matheus_vb@hotmail.com>
2018-01-26 12:57:25 -0200

    Changet the order of some checks.

--- R/HHJ.R
+++ R/HHJ.R
@@ -134,12 +134,31 @@
 
 
 HHJ <-
-function(data,statistic,R=100,nsteps=5L,type.est="bias.variance",type.optm=0,
-		 allow.parallel=TRUE,errorhandling="try",n.try=3,m.init="default",
+function(data,statistic,R=100L,nsteps=5L,type.est="bias.variance",type.optm=0,
+		 allow.parallel=TRUE,errorhandling="try",n.try=3L,m.init="default",
 		 type.sub.blocks="fast",seed=123,export,...) {
+
+R <- floor(R)
+if((!is.numeric(R)||(R <=0)))
+	stop("'R' must be positive integer")
+
 
 if((!is.numeric(nsteps)||(nsteps <=0)))
 	stop("nsteps must be a positive integer")
+nsteps <- floor(nsteps)
+
+if (type.est%in% c("bias.variance","one.sided.distribution","two.sided.distribution") ) {
+	switch(type.est,"bias.variance"={k <- 1}
+				   ,"one.sided.distribution"={k <- 2}
+				   ,"two.sided.distribution"={k <- 3}
+		  )
+}else stop("unrecognised value of 'type.est',accepted values are: 'bias.variance',
+		   'one.sided.distribution' and 'two.sided.distribution'")
+
+if(!is.numeric(type.optm)||(type.optm <0))
+	stop("type.otpm must be a positive integer; accepted values are:
+	0 for mean of the parameters vector or the index of the desired parameter
+	to optimize in the same order as provided by statistic")
 
 if(!is.logical(allow.parallel))
 	stop("allow.parallel must be logical (i.e.: TRUE/FALSE)")
@@ -148,15 +167,11 @@ if(allow.parallel)
 	if (foreach::getDoParRegistered()==FALSE)
 		stop("paralllel backend must be registered")
 
-if (! errorhandling%in% c("try","pass") )
+if (!errorhandling%in% c("try","pass") )
 	stop("unrecognised value of 'errorhandling',accepted values are: 'try' and 'pass'")
 
 if((errorhandling=="try")&&(!is.numeric(n.try)||(n.try <=0)))
 	stop("n.try must be a positive integer")
-
-if(!is.numeric(type.optm)||(type.optm <0))
-	stop("type.otpm must be a positive integer; accepted values are:
-	0 for mean of the parameters vector or the index of the desired parameter to optimize in the same order as provided by statistic")
 
 n <- if(is.vector(data)) length(data) else if(is.matrix(data)||is.data.frame(data)) nrow(data)
 
@@ -164,18 +179,12 @@ if(m.init!="default")
 	if(!(is.numeric(m.init)&&(m.init < n)&&(m.init > 0)))
 		stop("m.init must be a positive integer smaller than n. \nTip: you could also specify m.init='default' and in this case m.init=round(n^(1/3))")
 
+if ( !type.sub.blocks%in% c("fast","complete") )
+	stop("unrecognised value of 'type.sub.blocks',accepted values are: 'fast' and 'complete'")
+
 if(!is.numeric(seed))
 	stop('seed must be numeric')
 
-if (type.est%in% c("bias.variance","one.sided.distribution","two.sided.distribution") ) {
-	switch(type.est,"bias.variance"={k <- 1}
-				   ,"one.sided.distribution"={k <- 2}
-				   ,"two.sided.distribution"={k <- 3}
-		  )
-}else stop("unrecognised value of 'type.est',accepted values are: 'bias.variance', 'one.sided.distribution' and 'two.sided.distribution'")
-
-if ( !type.sub.blocks%in% c("fast","complete") )
-	stop("unrecognised value of 'type.sub.blocks',accepted values are: 'fast' and 'complete'")
 
 opt.l <- data.frame(iter=seq.int(from=0,to=nsteps),l.optm=c(if(m.init=="default")round(n^(1/3)) else round(m.init) ,rep(0,nsteps)))
 
