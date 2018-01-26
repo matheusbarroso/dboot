98e2359f83544f64d756b479f27da682367a4fe3
matheusbarroso <matheus_vb@hotmail.com>
2018-01-26 12:56:53 -0200

    Changed the order of some checks.

---
+++ R/dboot.R
@@ -0,0 +1,23 @@
+#'dboot: Dependent Bootstrap package
+#'
+#'
+#'A package for bootstrapping dependent data. The dboot
+#' package provides two main functions HHJ e LFL
+#'that computes the Moving Block Bootstrap optimal block
+#'length selection algorithms. The former an adapted version
+#' of Hall,
+#'Horowitz and Jing (1995) and the latter Lahiri, Furukawa
+#'and Lee (2007)
+#'
+#'@section dboot functions:
+#'The dboot functions: HHJ and LFL
+#'
+#'@author Matheus de Vasconcellos Barroso
+#'
+#'
+#'@docType package
+#'@name dboot
+#'
+#'
+
+NULL
